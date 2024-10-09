b82a58b43ef3a7b613f53d2e7efa09feded96d4e
matus <matusmonogram@gmaIl.com>
2024-10-09 09:25:35 +0200

    poznamky z cvika

--- src/sk/uniza/fri/aplikacia/Nehnutelnost.java
+++ src/sk/uniza/fri/aplikacia/Nehnutelnost.java
@@ -8,6 +8,8 @@ import sk.uniza.fri.struktura.Kluce;
  *
  * @author matus
  */
+//ulozit do viacej stromov lebo nimi netreba setrit
+    //konkretne 3 stromy v jednom parcely v druhom nehnutelnosti a v tretom oboje
 public class Nehnutelnost extends StromNode {
 
     public Nehnutelnost(int paCislo, String paPopis, StromNode[] paZoznam, Kluce paKluc) {
